2ee001b5a0c0aed7f888db8c2805d67c45d64163
Kannaphat-khwanon <Kannaphat.k@rmutsvmail.com>
2022-06-26 22:55:53 +0700

    Initial commit

---
+++ Lab6/ex_oop3.py
@@ -0,0 +1,29 @@
+"""
+Name : {kannaphat khwanon}
+ID: {364211760047}
+Group: {MIT212}
+"""
+
+class Teacher:
+    def __init__(self,name):
+        self.name = name
+    def introduce(self):
+        print(f'My name is {self.name}, I am Teacher.')
+
+class Student:
+    def __init__(self,name):
+        self.name = name
+    def introduce(self):
+        print(f'My name is {self.name}, I am Student.')
+
+mylist = []
+t = Teacher('Puriwat Lertkrai')
+s = Student('Jatuphon Chit')
+
+mylist.append(t)
+mylist.append(s)
+
+#print(len(mylist))
+
+for x in mylist:
+    x.introduce()
